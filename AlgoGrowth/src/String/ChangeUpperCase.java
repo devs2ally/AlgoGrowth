f66ab6a6749a52891186126d0ce73bd5bcf8075e
sally <devs2ally@gmail.com>
2025-08-09 16:59:18 +0900

    1-2 대소문자 변환

--- AlgoGrowth/src/String/ChangeUpperCase.java
+++ AlgoGrowth/src/String/ChangeUpperCase.java
@@ -2,40 +2,44 @@ package String;
 
 import java.util.*;
 
+/**
+ * 설명
+
+대문자와 소문자가 같이 존재하는 문자열을 입력받아 대문자는 소문자로 소문자는 대문자로 변환하여 출력하는 프로그램을 작성하세요.
+
+
+입력
+
+첫 줄에 문자열이 입력된다. 문자열의 길이는 100을 넘지 않습니다.
+
+문자열은 영어 알파벳으로만 구성되어 있습니다.
+
+
+출력
+
+첫 줄에 대문자는 소문자로, 소문자는 대문자로 변환된 문자열을 출력합니다. 
+ */
 public class ChangeUpperCase {
 
-	// 프로그래머스 방식
-	public int solution(String str, char t) {
-		int answer=0;
-		str = str.toUpperCase(); // 대문자
-		t = Character.toUpperCase(t); // 대문자
-		System.out.println(str+" "+t);
-		
-		for (int i = 0; i < str.length(); i++) {
-			if (str.charAt(i) == t) {
-				answer = str.charAt(i);
+	public String solution(String str) {
+		String answer="";
+		for (char x : str.toCharArray()) {
+			if (Character.isLowerCase(x)) {
+				answer += Character.toUpperCase(x);
+			} else {
+				answer += Character.toLowerCase(x);
 			}
 		}
 		
 		return answer;
 	}
 	
-	// 백준 방식
 	public static void main(String[] args) {
 		ChangeUpperCase T = new ChangeUpperCase();
 		Scanner sc = new Scanner(System.in);
-		
-		// 입력
-		String str = sc.next(); // 문자열을 읽음
-		char c = sc.next().charAt(0); //문자 하나
-		System.out.println(T.solution(str, c));
-		
+		String str = sc.next(); 
+		System.out.println(T.solution(str));
 		sc.close();
 	}
-	
-	
-	
-	
-	
 	
 }
