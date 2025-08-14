9529625bc64f01ca54a41e96366a92bcdd6fedca
sally <devs2ally@gmail.com>
2025-08-14 14:40:16 +0900

    1-6 중복제거

--- AlgoGrowth/src/String/StringReverser.java
+++ AlgoGrowth/src/String/StringReverser.java
@@ -8,20 +8,20 @@ public class StringReverser {
 	public ArrayList<String> solution(int num, String[] str) {
 		ArrayList<String> answer = new ArrayList<>();
 		// StringBuilder reverse
-//		for(String x : str) {
-//			String tmp = new StringBuilder(x).reverse().toString(); 
-//			answer.add(tmp);
-//		}
+		// for(String x : str) {
+		// String tmp = new StringBuilder(x).reverse().toString();
+		// answer.add(tmp);
+		// }
 		// str[] = good, time
 		// x = good 먼저
 		// s = g/o/o/d
 		for (String x : str) {
 			char[] s = x.toCharArray();
-			int lt = 0, rt = x.length()-1;
-			while(lt < rt) { // 0 < 4
+			int lt = 0, rt = x.length() - 1;
+			while (lt < rt) { // 0 < 4
 				char tmp = s[lt]; // tmp = g
 				s[lt] = s[rt]; // s = d o o d
-				s[rt] = tmp;   // s = d o o g
+				s[rt] = tmp; // s = d o o g
 				lt++; // 1
 				rt--; // 3
 			}
@@ -30,8 +30,7 @@ public class StringReverser {
 		}
 		return answer;
 	}
-	
-	
+
 	public static void main(String[] args) {
 		StringReverser T = new StringReverser();
 		Scanner sc = new Scanner(System.in);
@@ -43,7 +42,7 @@ public class StringReverser {
 		for (String x : T.solution(num, str)) {
 			System.out.println(x);
 		}
-		
+
 		sc.close();
 	}
 
