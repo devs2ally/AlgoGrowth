9b8993e068ce3468ad8316aa44f3ce9d65083355
sally <devs2ally@gmail.com>
2025-09-29 13:01:30 +0900

    숫자만 추출 1-9 추가 및 팰린드롬 설명 수정

---
+++ AlgoGrowth/src/String/숫자만추출_1_9.java
@@ -0,0 +1,53 @@
+package String;
+
+import java.util.Scanner;
+
+public class 숫자만추출_1_9 { 
+    /**
+     * 설명
+
+        문자와 숫자가 섞여있는 문자열이 주어지면 그 중 숫자만 추출하여 그 순서대로 자연수를 만듭니다.
+
+        만약 “tge0a1h205er”에서 숫자만 추출하면 0, 1, 2, 0, 5이고 이것을 자연수를 만들면 1205이 됩니다.
+
+        추출하여 만들어지는 자연수는 100,000,000을 넘지 않습니다.
+
+
+        입력
+
+        첫 줄에 숫자가 섞인 문자열이 주어집니다. 문자열의 길이는 100을 넘지 않습니다.
+
+
+        출력
+
+
+     */
+    public int solution(String str) {
+        String answer = "";
+
+        // // 1. 아스키 문자
+        // for (char x : str.toCharArray()) {
+        //     if (x >=48 && x <= 57) {
+        //         answer = answer * 10 + (x - 48);
+        //     }
+        // }
+
+        // 2. Character.isDigit() 메서드
+        for (char x : str.toCharArray()) {
+            if (Character.isDigit(x)) {
+                answer += x;
+            }
+        }
+
+        return Integer.parseInt(answer);
+    }
+
+    public static void main(String[] args) {
+        숫자만추출_1_9 T = new 숫자만추출_1_9();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+
+        System.out.println(T.solution(str));
+        sc.close();
+    }
+}
